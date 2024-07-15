4c34dc5ac0e45d88d24e18efa9e1e1ac823ac24c
AlexanderVasilenko <alexandervjr1@gmail.com>
2024-07-15 14:11:22 +0300

    added conditional rendering to series name

--- src/Components/Main/HomePage/Book/Book.tsx
+++ src/Components/Main/HomePage/Book/Book.tsx
@@ -5,7 +5,7 @@ function Book(book: BookModel): JSX.Element {
     return (
         <div className="Book">
             <img src={require(`../../../../Assets/Images/` + book.coverImage)} />
-            <span className="title">{book.title}</span>
+            <span className="title">{book.series.name ? `${book.series.name} - ` : ''}{book.title}</span>
             <span className="author">by {book.author}</span>
         </div>
     );
