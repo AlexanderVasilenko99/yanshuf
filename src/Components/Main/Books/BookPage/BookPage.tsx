5bd9f558e1781e53a2e0c88da7afa48fda364b9c
AlexanderVasilenko <alexandervjr1@gmail.com>
2024-07-19 11:19:35 +0300

    fixed image import on bookPage

--- src/Components/Main/Books/BookPage/BookPage.tsx
+++ src/Components/Main/Books/BookPage/BookPage.tsx
@@ -1,23 +1,26 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./BookPage.scss";
-import { useEffect } from "react";
-import bookServices from "../../../../Services/BookServices";
 import BookModel from "../../../../Models/BookModel";
+import bookServices from "../../../../Services/BookServices";
+import "./BookPage.scss";
 
 function BookPage(): JSX.Element {
     const params = useParams();
-
+    const [book, setBook] = useState<BookModel>();
     useEffect(() => {
         bookServices.getBookByAuthorAndTitle(params.bookIdentifier)
-            .then((book: BookModel) => {console.log(book)})
-            .catch((err: any) => { console.log(err.message) })
+            .then((b: BookModel) => {
+                setBook(b);
+            })
+            .catch((err: any) => { console.log(err.message) });
     }, [])
 
     return (
         <div className="BookPage">
             <div className="initialInfo">
                 <div>
-                    {/* <img src={} /> */}
+                    {book && <img src={require(`../../../../Assets/Images/` + book.coverImage)} />}
+                    {/* <img src={require("../../../../../Assets/Images/Fleet/Vehicles/" + props.vehicleCategoryImgName)} alt="" /> */}
                 </div>
                 <div>{params.bookIdentifier}</div>
             </div>
