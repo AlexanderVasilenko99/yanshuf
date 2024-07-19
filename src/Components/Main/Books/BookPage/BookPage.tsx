f16bd402ddc57006fd0d9fe6090a3f4edf6ec266
AlexanderVasilenko <alexandervjr1@gmail.com>
2024-07-19 14:49:30 +0300

    a

--- src/Components/Main/Books/BookPage/BookPage.tsx
+++ src/Components/Main/Books/BookPage/BookPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import BookModel from "../../../../Models/BookModel";
 import bookServices from "../../../../Services/BookServices";
 import "./BookPage.scss";
@@ -9,21 +9,36 @@ function BookPage(): JSX.Element {
     const [book, setBook] = useState<BookModel>();
     useEffect(() => {
         bookServices.getBookByAuthorAndTitle(params.bookIdentifier)
-            .then((b: BookModel) => {
-                setBook(b);
-            })
+            .then((b: BookModel) => { setBook(b) })
             .catch((err: any) => { console.log(err.message) });
     }, [])
 
     return (
         <div className="BookPage">
-            <div className="initialInfo">
-                <div>
-                    {book && <img src={require(`../../../../Assets/Images/BookCovers/` + book.coverImage)} />}
+            {book && <div className="initialInfo">
+                <div className="left-col">
+                    {<img src={require(`../../../../Assets/Images/BookCovers/` + book.coverImage)} />}
                 </div>
-                <div>{params.bookIdentifier}</div>
-            </div>
-        </div>
+                <div className="right-col">
+                    <p className="header">
+                        <span className="book-title">{book.title} </span>
+                        <span className="series">
+                            {book.series && `(number ${book.series.orderInSeries} in the `}
+                            {book.series && <NavLink to='' >{book.series.name}</NavLink>}
+                            {book.series && ` series)`}
+                        </span>
+                    </p>
+                    <p className="author">by {<NavLink to='' >{book.author}</NavLink>}</p>
+                    <p className="description">{book.description}</p>
+                    <p className="language-rating">
+                        <span className="language">Language: {book.language}</span>
+                        <span className="rating">Rating: {book.rating}</span>
+
+                    </p>
+                    <p className="price">Available for just <button>{book.price}&#8362;</button>!</p>
+                </div>
+            </div>}
+        </div >
     );
 }
 
