705776c93a4d355ca7f73ee5ae394fc60cd1de3b
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-08-31 14:58:18 +0300

    completed marquee and footer language integration

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
+import { useTranslation } from "react-i18next";
 import "./Footer.scss";
 
 function Footer(): JSX.Element {
+    const { t } = useTranslation();
     return (
         <div className="Footer">
-            <p>©Yanshuf books ltd. All rights reserved. Made possible by Alexander Vasilenko.</p>
+            <p>{t('©Yanshuf books ltd. All rights reserved. Made possible by Alexander Vasilenko.')}</p>
         </div>
     );
 }
--- src/Components/Marquee/Marquee.tsx
+++ src/Components/Marquee/Marquee.tsx
@@ -1,10 +1,12 @@
+import { useTranslation } from "react-i18next";
 import "./Marquee.scss";
 import MarqueeComponent from "react-fast-marquee";
 function Marquee(): JSX.Element {
+    const { t } = useTranslation();
     return (
         <MarqueeComponent className="Marquee">
-            <div className="marquee-text">Ra'ananas most iconic bookstore now with an online presence!</div>
-            <div className="marquee-text">New in: Eli's picks of the month for August 2024!</div>
+            <div className="marquee-text">{t("Ra'ananas most iconic bookstore now with an online presence!")}</div>
+            <div className="marquee-text">{t("New in: Eli's picks of the month for August 2024!")}</div>
         </MarqueeComponent >
     );
 }
