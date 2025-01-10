30dde3a21dffa7bd39f84c01226c95ce29853f96
Alexander Vasilenko <alexandervjr1@gmail.com>
2025-01-10 20:05:30 +0200

    added sales text

--- src/Components/Main/Sales/Sales.tsx
+++ src/Components/Main/Sales/Sales.tsx
@@ -5,7 +5,16 @@ function Sales(): JSX.Element {
     const { t } = useTranslation();
     return (
         <div className="Sales">
-            <h3>{t('Sales')}</h3>
+
+            <h3>{t(`Ongoing sale on the books outside!`)}</h3>
+            <p>{t(`Best Sellers - each book for 20₪`)}
+                <br />
+                {t(`or`)}
+                <br /> 
+                {t(`10 books for 100₪`)}
+            </p>
+            {/* <h3>call to action</h3> */}
+
         </div>
     );
 }
